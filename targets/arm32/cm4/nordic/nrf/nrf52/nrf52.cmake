247de4024480f2e3199cc8373beb8a06d41aa03d
Carlos van Rooijen <info@carlosvanrooijen.nl>
2016-12-02 16:04:52 +0100

    changed folder structure to a more depended way. Common files go now in parent folders. Also flashscripts are now separate

--- targets/arm32/cm4/nordic/nrf/nrf52/nrf52.cmake
+++ targets/arm32/cm4/nordic/nrf/nrf52/nrf52.cmake
@@ -10,11 +10,12 @@ enable_language(C)
 
 SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
 
-link_directories(targets/nordic/nrf52/deps)
+link_directories(${toolchain}/targets/arm32/cm4/nordic/nrf/nrf52/deps)
 
 include_directories(
-	targets/nordic/nrf52/deps
-        /home/carlos/gits/Kvasir/Lib
+        ${toolchain}/targets/arm32/cm4/nordic/nrf/nrf52/deps
+        ${toolchain}/targets/arm32/cm4/nordic/nrf/deps
+        ${toolchain}/targets/arm32/cm4/deps
         )
 add_definitions(-DNRF52)
 
@@ -31,21 +32,11 @@ set(CMAKE_C_FLAGS
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Tnrf52_xxaa.ld")
 
 add_executable(${PROJECT_NAME}.elf
-        targets/nordic/nrf52/deps/system_nrf52.c
-        targets/nordic/nrf52/deps/gcc_startup_nrf52.s
+        ${toolchain}/targets/arm32/cm4/nordic/nrf/nrf52/deps/system_nrf52.c
+        ${toolchain}/targets/arm32/cm4/nordic/nrf/nrf52/deps/gcc_startup_nrf52.s
 	${sources}
 	)
 
 add_custom_target(${PROJECT_NAME}.hex DEPENDS ${PROJECT_NAME}.elf COMMAND ${OBJCOPY} -Oihex ${PROJECT_NAME}.elf ${PROJECT_NAME}.hex)
 add_custom_target(${PROJECT_NAME}.bin DEPENDS ${PROJECT_NAME}.elf COMMAND ${OBJCOPY} -Obinary ${PROJECT_NAME}.elf ${PROJECT_NAME}.bin)
 
-IF(nrfjprog)
-	add_custom_target(flash DEPENDS ${PROJECT_NAME}.hex 
-	        COMMAND nrfjprog -f NRF52 --program ${PROJECT_NAME}.hex --chiperase --verify
-        	COMMAND nrfjprog -f NRF52 --reset
-	)
-else()
-	add_custom_target(flash
-		COMMAND echo "nrfjprog not in path. Add to path and run cmake again."
-	)
-endif()
