1f48e9b84eda040b0d43b653fc19b4a8550e6434
ManDeJan <janhlsm@gmail.com>
2017-02-12 12:29:28 +0100

    We can make a working bin now but looking for a cleaner way

--- targets/arm32/cm3/atmel/sam3x/deps/source/templates/gcc/startup_sam3x.c
+++ targets/arm32/cm3/atmel/sam3x/deps/source/templates/gcc/startup_sam3x.c
@@ -46,7 +46,65 @@
 
 #include "exceptions.h"
 #include "sam3xa.h"
-#include "system_sam3x.h"
+//#include "system_sam3x.h"
+    void hang() { while (1); }
+
+    static void emptyHandler() { }
+    static void dummyHandler() { hang(); }
+
+    void NMI_Handler()        __attribute__((weak, alias("dummyHandler")));
+    void HardFault_Handler()  __attribute__((weak, alias("dummyHandler")));
+    void MemManage_Handler()  __attribute__((weak, alias("dummyHandler")));
+    void BusFault_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void UsageFault_Handler() __attribute__((weak, alias("dummyHandler")));
+    void SVC_Handler()        __attribute__((weak, alias("dummyHandler")));
+    void DebugMon_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void PendSV_Handler()     __attribute__((weak, alias("dummyHandler")));
+
+    void SysTick_Handler() __attribute__((weak, alias("emptyHandler")));
+
+    void ADC_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void CAN0_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void CAN1_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void DACC_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void DMAC_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void EFC0_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void EFC1_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void EMAC_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void HSMCI_Handler()  __attribute__((weak, alias("dummyHandler")));
+    void PIOA_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void PIOB_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void PIOC_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void PIOD_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void PMC_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void PWM_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void RSTC_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void RTC_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void RTT_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void SMC_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void SPI0_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void SSC_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void SUPC_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void TC0_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC1_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC2_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC3_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC4_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC5_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC6_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC7_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TC8_Handler()    __attribute__((weak, alias("dummyHandler")));
+    void TRNG_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void TWI0_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void TWI1_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void UART_Handler()   __attribute__((weak, alias("dummyHandler")));
+    void UOTGHS_Handler() __attribute__((weak, alias("dummyHandler")));
+    void USART0_Handler() __attribute__((weak, alias("dummyHandler")));
+    void USART1_Handler() __attribute__((weak, alias("dummyHandler")));
+    void USART2_Handler() __attribute__((weak, alias("dummyHandler")));
+    void USART3_Handler() __attribute__((weak, alias("dummyHandler")));
+    void WDT_Handler()    __attribute__((weak, alias("dummyHandler")));
+
 
 /* Initialize segments */
 extern uint32_t _sfixed;
--- targets/arm32/cm3/atmel/sam3x/sam3x8e/sam3x8e.cmake
+++ targets/arm32/cm3/atmel/sam3x/sam3x8e/sam3x8e.cmake
@@ -15,9 +15,13 @@ include_directories(
         ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps
         ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/include
         ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/source/templates
+        ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/source/templates/gcc
+        ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/source/templates
         ${toolchain}/targets/arm32/cm3/atmel/sam3x/sam3x8e/deps
         )
 add_definitions(-D__SAM3X8E__)
+add_definitions(-DDONT_USE_CMSIS_INIT)
+
 
 set(CMAKE_CXX_FLAGS
         #"${CMAKE_CXX_FLAGS} -mcpu=cortex-m4 -mthumb -mabi=aapcs -mfpu=fpv4-sp-d16 -mfloat-abi=hard"
@@ -29,11 +33,11 @@ set(CMAKE_C_FLAGS
 )
 
 #Ik geloof dat dat het goeie flash script is
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Tflash.ld")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}-Tflash.ld -Wl,--gc-sections -Wl,--entry=Reset_Handler")
 
 add_executable(${PROJECT_NAME}.elf
+        ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/source/templates/gcc/startup_sam3x.c
         # ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/include/sam3x8e.h
-        # ${toolchain}/targets/arm32/cm3/atmel/sam3x/deps/source/templates/gcc/startup_sam3x.c
 	${sources}
 	)
 
