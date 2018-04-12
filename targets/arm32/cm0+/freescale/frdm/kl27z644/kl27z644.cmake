829e4a1ac37c93e5e8c07666c475df42fc313ed9
Chris Smeele <cj.smeele@xs4all.nl>
2018-04-12 15:46:02 +0200

    Added KL27Z644 CMakeLists.txt

---
+++ targets/arm32/cm0+/freescale/frdm/kl27z644/kl27z644.cmake
@@ -0,0 +1,36 @@
+enable_language(ASM)
+enable_language(C)
+
+SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
+
+link_directories(${toolchain}/targets/arm32/cm0+/freescale/frdm/kl27z644/deps)
+
+include_directories(${toolchain}/targets/arm32/cm0+/freescale/frdm/kl27z644/deps)
+#add_definitions(-D__START=main -D__NO_SYSTEM_INIT)
+#add_definitions(-D__NO_SYSTEM_INIT)
+add_definitions(-DNDEBUG
+                -DCPU_MKL27Z32VLH4
+                -DFRDM_KL27Z
+                -DFREEDOM)
+
+#set(compile_flags "-Os -mcpu=cortex-m0plus -mthumb -fomit-frame-pointer")
+#set(compile_flags "-Os -mcpu=cortex-m0plus  -mthumb  -MMD  -MP  -Wall  -fno-common  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin  -mapcs  -std=gnu99 ${compile_flags}")
+set(compile_flags "-Os -mcpu=cortex-m0plus  -mthumb  -MMD  -MP  -Wall  -fno-common  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin  -mapcs  ${compile_flags}")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${compile_flags} -fno-exceptions")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${compile_flags}")
+
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --specs=nano.specs  -lm  -Wall  -fno-common  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin  -mthumb  -mapcs  -Xlinker --gc-sections  -Xlinker -static  -Xlinker -z  -Xlinker muldefs  -Xlinker --defsym=__stack_size__=0x300  -Xlinker --defsym=__heap_size__=0x200 -TMKL27Z64xxx4_flash.ld")
+
+#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -TMKL27Z64xxx4_flash.ld")
+
+add_executable(${PROJECT_NAME}.elf
+        ${toolchain}/targets/arm32/cm0+/freescale/frdm/kl27z644/deps/startup_MKL27Z644.S
+        ${toolchain}/targets/arm32/cm0+/freescale/frdm/kl27z644/deps/startup.c
+        ${toolchain}/targets/arm32/cm0+/freescale/frdm/kl27z644/deps/system_MKL27Z644.c
+        ${sources}
+)
+
+add_custom_target(${PROJECT_NAME}.hex DEPENDS ${PROJECT_NAME}.elf COMMAND ${OBJCOPY} -Oihex ${PROJECT_NAME}.elf ${PROJECT_NAME}.hex)
+add_custom_target(${PROJECT_NAME}.bin DEPENDS ${PROJECT_NAME}.elf COMMAND ${OBJCOPY} -Obinary ${PROJECT_NAME}.elf ${PROJECT_NAME}.bin)
